bd15bd5d766cd2eb262001ac672242b65e3894ab
Alex Driedger <aldriedger@gmail.com>
2022-03-13 23:55:35 -0700

    Use underscore

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-      name='sapai-gym',
+      name='sapai_gym',
       version='0.1',
-      packages=['sapai-gym'],
+      packages=['sapai_gym'],
       install_requires=[
           "sapai @ git+https://github.com/manny405/sapai.git@main",
           "gym~=0.22.0",
