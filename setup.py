a2cf325dc60d204150fb8074c1e874cb2f0c8798
Alex Driedger <aldriedger@gmail.com>
2022-03-14 00:06:57 -0700

    Remvoe numpy dep

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
       install_requires=[
           "sapai @ git+https://github.com/manny405/sapai.git@main",
           "gym~=0.22.0",
-          "numpy~=1.21.5",
+          # "numpy~=1.21.5",
           "sklearn"
       ],
       data_files=[],
