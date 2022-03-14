17ee068cd84f28d7adf6272503ba41ef1c69003e
Alex Driedger <aldriedger@gmail.com>
2022-03-13 23:29:30 -0700

    get rid of sapai dep

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
       version='0.1',
       packages=['sapai-gym'],
       install_requires=[
-          'git+https://github.com/manny405/sapai.git',
+          # 'git+https://github.com/manny405/sapai.git',
           'gym~=0.22.0'
           'numpy~=1.22.2'
           'sklearn'
