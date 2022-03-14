a129548953b4123c51fe1f240e4b587e906cee2e
Alex Driedger <aldriedger@gmail.com>
2022-03-13 23:41:40 -0700

    Use quotes on install requirements

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
       packages=['sapai-gym'],
       install_requires=[
           # 'git+https://github.com/manny405/sapai.git',
-          'gym~=0.22.0'
-          'numpy~=1.22.2'
-          'sklearn'
+          "gym~=0.22.0"
+          "numpy~=1.22.2"
+          "sklearn"
       ],
       data_files=[],
 )
