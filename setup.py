6b9fc831164d5710f6a86ffc4f29e8a204fd0b8f
Alex Driedger <aldriedger@gmail.com>
2022-03-13 23:45:01 -0700

    Added commas

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
       packages=['sapai-gym'],
       install_requires=[
           # 'git+https://github.com/manny405/sapai.git',
-          "gym>=0.22.0"
-          "numpy>=1.22.2"
+          "gym~=0.22.0",
+          "numpy~=1.22.2",
           "sklearn"
       ],
       data_files=[],
