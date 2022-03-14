bc1e77814c5466499694aacafb4cc973a8408a19
Alex Driedger <aldriedger@gmail.com>
2022-03-13 23:18:03 -0700

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+      name='sapai-gym',
+      version='0.1',
+      packages=['sapai-gym'],
+      install_requires=[
+          'git+https://github.com/manny405/sapai.git',
+          'gym~=0.22.0'
+          'numpy~=1.22.2'
+          'sklearn'
+      ],
+)
