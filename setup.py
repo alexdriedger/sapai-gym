f057f8bc819f3fe5ef2cd4166fbb734470a38ddf
Alex Driedger <aldriedger@gmail.com>
2022-04-04 16:41:12 -0700

    Use setup to find packages

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
       name='sapai_gym',
       version='0.1',
-      packages=['sapai_gym'],
+      packages=find_packages(),
       install_requires=[
           "sapai @ git+https://github.com/manny405/sapai.git@main",
           "gym~=0.21.0",
