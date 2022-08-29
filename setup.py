954f14af5c827a8138a4613e2b66f1008c3d7c59
André Pedersen <andrped94@gmail.com>
2022-08-29 18:56:04 +0200

    Workflow CI + wheel (#8)
    * added workflow CI
    
    * fixed typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
       name='sapai_gym',
-      version='0.1',
+      version='0.1.0',
       packages=find_packages(),
       install_requires=[
           "sapai @ git+https://github.com/manny405/sapai.git@main",
